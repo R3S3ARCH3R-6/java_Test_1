a08851ad078a995a67a1282a9a1bb1390ee07ec0
R3S3ARCH3R-6 <R3S3ARCH3R-6@users.noreply.github.com>
2020-09-21 14:44:44 -0500

    adding 'my_First_Java_Project' to the repo

---
+++ my_First_Java_Project/src/myPack/MyFirstJavaClass.java
@@ -0,0 +1,22 @@
+package myPack;
+
+//class name and file name should be same
+public class MyFirstJavaClass {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+/*
+ * compile a java file using
+ * javac Filename.java
+ * and run it using 
+ * java ClassName
+ * 
+ * and if you have to place your java class in a package, and run command below
+ * javac -d . FileName.java
+ * java packagename.ClassName
+ * 
+ */
+}
+
